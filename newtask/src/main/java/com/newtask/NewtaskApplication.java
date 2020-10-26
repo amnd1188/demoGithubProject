b83b80597704ef8139ec4501b99f7b4a55b2f50b
amnd1188 <amandadasanayaka4336@gmail.com>
2020-10-26 11:22:14 +0530

    error fix

---
+++ newtask/src/main/java/com/newtask/NewtaskApplication.java
@@ -0,0 +1,13 @@
+package com.newtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtaskApplication.class, args);
+	}
+
+}
